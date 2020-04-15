d021517ac52d16639014ccced41855470d2f9820
Allister <ageebernal@aol.com>
2020-04-15 00:07:06 -0500

    change api.py file

--- api.py
+++ api.py
@@ -4,31 +4,36 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones WHo Walk Away from Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Fest',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
+artists = [
+    {'name': 'Martin Garrix'},
+    {'name': 'Jonas Blue'},
+    {'name': 'Diplo'}
 ]
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Info App</h1><p>This site is for the info API</p>"
+    return "<h1>Info App</h1><p>This site is for the info EDC API</p>"
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/api/v1/resources/artists/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    return jsonify(artists)
+
+@app.route('/api/v1/resources/artists', methods=['GET'])
+def api_id():
+
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    results = []
+
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+
+    return jsonify(results);
 
 app.run()
